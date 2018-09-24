e996135a849df6c0216d2346c013f9572ec45007
Tempeus <36718822+Tempeus@users.noreply.github.com>
2018-09-24 15:29:38 -0400

    Update 1_4.java

--- 1_4.java
+++ 1_4.java
@@ -30,7 +30,7 @@ public class Bounce extends GraphicsProgram {
 		add(ball);
 		
 		//line	property
-		GLine ground = new GLine(0,750,1000,750); // this value will be affected if changing the size of canvas
+		GLine ground = new GLine(0,600,1000,600); // this value will be affected if changing the size of canvas
 		add(ground);
 		
 		//movement mechanic of the ball (will be affected if changing the size of canvas)
@@ -38,21 +38,19 @@ public class Bounce extends GraphicsProgram {
 			if (!dirUp) { // which direction the ball is dropping
 				h = h0 - 0.5 * G * Math.pow(t, 2);
 				if (h <= 0) { // ground impact
-					println("GROUND"); // indication that this if statement is triggered
+					//println("GROUND"); // indication that this if statement is triggered
 					h0 = h;
-					
+					initialUpPosition = h;
 					dirUp = true; // changes the direction of the ball
 					t = 0;
 					vt = vt * Math.sqrt(1 - e);
 				}
-				else {
+			}
+			else {
 				h = initialUpPosition + vt * t - 0.5 * G * Math.pow(t, 2); // for some reason the value here is not changed from the if statement above
-				}
-				
 				if (h > h0) {
 					h0 = h;
 				}
-				
 				else {
 					dirUp = false;
 					t = 0;
@@ -63,7 +61,7 @@ public class Bounce extends GraphicsProgram {
 			
 			t+= intervalTime;
 			totalTime += intervalTime;
-			pause(intervalTime);
+			pause(intervalTime*100);
 			
 			//tracer property
 			add(new GOval(xPos+30,570-h,1,1));
@@ -71,6 +69,7 @@ public class Bounce extends GraphicsProgram {
 		}
 	}
 }
+
 
 /* problems:
  * []the ball isn't moving vertically
