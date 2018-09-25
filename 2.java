e28c6ba529f0acb89a5ded758c2c1a5046f6089a
Tempeus <36718822+Tempeus@users.noreply.github.com>
2018-09-25 16:06:22 -0400

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,18 @@
+import acm.graphics.GLine;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+import java.awt.Color;
+import java.awt.color.*;
+
+public class Li_260862327_A2 extends Thread {
+	
+	public class gBall(double Xi, double Yi,double bSize, Color bColor, double bLoss, double bVel) {
+		this.Xi = Xi;
+		this.Yi = Yi;
+		this.bSize = bSize;
+		this.bColor = bColor;
+		this.bLoss = bLoss;
+		this.bVel = bVel;
+	}
+}
