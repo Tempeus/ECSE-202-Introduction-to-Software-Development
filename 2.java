cd824df1e72858c9bce346c1345a56143617551b
Tempeus <36718822+Tempeus@users.noreply.github.com>
2018-09-25 16:05:53 -0400

    Update 2.java

--- 2.java
+++ 2.java
@@ -1,1 +1,22 @@
+import acm.program.GraphicsProgram;
 
+public class bSim extends GraphicsProgram {
+	private static final int WIDTH = 1200;
+	private static final int HEIGHT = 600;
+	private static final int OFFSET = 200;
+	private static final int NUMBALLS = 100;
+	private static final double MINSIZE = 3;
+	private static final double MAXSIZE = 20;
+	private static final double XMIN = 10;
+	private static final double XMAX = 50;
+	private static final double YMIN = 50;
+	private static final double YMAX = 100;
+	private static final double EMIN = 0.1;
+	private static final double EMAX = 0.3;
+	private static final double VMIN = 0.5;
+	private static final double VMAX = 3.0;
+	
+	public void run() {
+		
+	}
+}
