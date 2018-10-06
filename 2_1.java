92c131ca0c290530c35c43abecb3b379eaa9368d
Tempeus <36718822+Tempeus@users.noreply.github.com>
2018-10-06 17:09:25 -0400

    Update 2_1.java

--- 2_1.java
+++ 2_1.java
@@ -1,12 +1,14 @@
+import java.awt.Color;
 import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class bSim extends GraphicsProgram {
 	//required parameters for the program
 	private static final int WIDTH = 1200;
 	private static final int HEIGHT = 600;
 	private static final int OFFSET = 200;
-	private static final int NUMBALLS = 100;
+	private static final int NUMBALLS = 100;  // change to 100
 	private static final double MINSIZE = 3;
 	private static final double MAXSIZE = 20;
 	private static final double XMIN = 10;
@@ -18,10 +20,26 @@ public class bSim extends GraphicsProgram {
 	private static final double VMIN = 0.5;
 	private static final double VMAX = 3.0;
 	
+	RandomGenerator rgen = new RandomGenerator();
+		
 	public void run() {
 		//set up display, create and start multiple instances of gball
-		this.resize(WIDTH,800);
+		this.resize(WIDTH, HEIGHT + OFFSET);
 		GLine ground = new GLine(0,HEIGHT,WIDTH,HEIGHT);
 		add(ground);
+		
+		
+		for(int i = 0; i < NUMBALLS; i++) {
+			double iX = rgen.nextDouble(XMIN,XMAX);
+			double iY = rgen.nextDouble(YMIN,YMAX);
+			double iSize = rgen.nextDouble(MINSIZE,MAXSIZE);
+			Color iColor = rgen.nextColor();
+			double iE = rgen.nextDouble(EMIN,EMAX);
+			double iV = rgen.nextDouble(VMIN,VMAX);
+			
+			gBall iball = new gBall(iX *10,iY * 10,iSize ,iColor ,iE ,iV);
+			add(iball.myBall);
+			iball.start();
+		}
 	}
 }
