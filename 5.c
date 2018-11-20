c98b88d6c225b3dcce9a21c1d9c7e75be082900f
Tempeus <36718822+Tempeus@users.noreply.github.com>
2018-11-20 13:15:36 -0500

    Update 5.c

--- 5.c
+++ 5.c
@@ -3,26 +3,25 @@
  *
  *  Created on: Nov 14, 2018
  *      Author: Kevin
+ *  Student ID: 260862327
  */
 #include <stdio.h>
 #include <stdlib.h>
 
 
-void dec2base(int input, int base, char* str);
+void dec2base(int input, int base, char *str);
 void revStr(char *str, int length);
 
-
+int number;
+int base;
 int main(void){
-	int number;
-	int base;
 	int quotient = number;
 	int n = 0;
 	char array[n+1];
 
-
+	
 	printf("dec2base ");
 	scanf("%d %d", &number,&base);
-
 
 	if (number < 0 || number > 2147483647){
 		printf("\nError: Number must be of range [0, 2147483647].");
@@ -40,48 +39,44 @@ int main(void){
 			quotient = quotient / base;
 			n ++;
 		}
-		dec2base(number, base, *array);
-		printf("%d\n", number);
-		printf("%d\n",base);
+		dec2base(number, base, array);
 	}
 }
 
 
-void dec2base(int input, int base, char* str){
+void dec2base(int input, int base, char *str){
 	int remainder;
 	int i = 0;
+	char reChar;
 
 
 	while (input != 0){
 		remainder = input % base;
 		
 		if (remainder < 10){
-			remainder = '0' + remainder;
+			reChar = '0' + remainder;
 		}
 		
 		if (remainder >= 10){
-			remainder = 'A' + (remainder - 10);
+			reChar = 'A' + (remainder - 10);
 		}
 
 
 		//add to array
-		str[i] = remainder;
+		str[i] = reChar;
 
 
 		input = input / base;
 		i++;
-	}
+	
 	revStr(str,i);
-	}
 }
 
 void revStr(char *str, int length){
-	char* str = (char*)malloc(sizeof(char)*length);
+	char* Rstr = (char*)malloc(sizeof(char)*length);
 	int i;
 	for(i = 0; i < length; i++){
-		str[i] = str[length - i - 1];
+		Rstr[i] = str[length - i - 1];
 	}
-	for(i=0; i < length; i ++){
-		printf("%c",str[i]);
-	}
+	printf("The Base-%d form of %d is: %s",base, number, Rstr);
 }
